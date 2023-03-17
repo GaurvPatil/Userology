49ebca7e9c46fedeb139b2062bff5165e7333157
Gaurav Patil <gaurvpatil17@gmail.com>
2023-03-17 17:19:07 +0530

    modal complete

---
+++ src/Modal.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export default function Modal() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
+  return (
+    <>
+      <button onClick={toggleModal} className="btn-modal">
+        Open
+      </button>
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <button className="close-modal" onClick={toggleModal}>
+              {" "}
+              Confirm{" "}
+            </button>
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
